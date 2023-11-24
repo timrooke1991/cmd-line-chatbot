c783910e38319795d84a28bbb441502f5c85aa3e
Tim Rooke <timrooke1991@gmail.com>
2023-11-24 15:03:53 +0000

    Langchain cmd line chatbot loading message history from file

---
+++ main.py
@@ -0,0 +1,38 @@
+from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.prompts import (
+    HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
+)
+
+load_dotenv()
+
+chat = ChatOpenAI()
+
+memory = ConversationBufferMemory(
+    chat_memory=FileChatMessageHistory("messages.json"),
+    memory_key="messages", 
+    return_messages=True
+)
+
+prompt = ChatPromptTemplate(
+    input_variables=["content", "messages"],
+    messages=[
+        MessagesPlaceholder(variable_name="messages"),
+        HumanMessagePromptTemplate.from_template("{content}")
+    ]
+)
+
+chain = LLMChain(
+    llm=chat,
+    prompt=prompt,
+    memory=memory,
+)
+
+while True:
+    content = input(">> ")
+
+    result = chain({"content": content})
+
+    print(result["text"])
